329f5cd8351eb908da70abd43ce3d91506a4cbba
den1s0v <hello-lumen@ya.ru>
2019-11-25 21:12:15 +0400

    "Рефакторинг" правил: префикс теперь обязятелен

--- rule.swrl.py
+++ rule.swrl.py
@@ -4,7 +4,7 @@
 # BeforeActTransitive
 c_schema:Act(?b) ^ c_schema:Act(?c) ^ c_schema:Act(?a) ^ c_schema:beforeAct(?a, ?b) ^ c_schema:beforeAct(?b, ?c) -> c_schema:beforeAct(?a, ?c)
 
-
+# 1
 # ActBeforeStartOfBlockError
 c_schema:Context(?c) 
 c_schema:Block(?block) 
@@ -21,12 +21,12 @@ c_schema:before(?act, ?act1)
 
 c_schema:hasIndex(?c, ?index)
 c_schema:hasOrigin(?act, ?stmt) 
-hasSource(?stmt, ?src) 
-hasLocationSuffix(?stmt, ?stlbl) 
+c_schema:hasSource(?stmt, ?src) 
+c_schema:hasLocationSuffix(?stmt, ?stlbl) 
 swrlb:stringConcat(?msg, "ActBeforeStartOfBlockError: ", ?src, ?stlbl, "#", ?index, " is placed before start of block.")
  -> c_schema:message(c_schema:ERRORS, ?msg)
 
-
+# 2
 # ActAfterEndOfBlockError
 c_schema:Context(?c) 
 c_schema:Block(?block) 
@@ -43,30 +43,31 @@ c_schema:before(?end, ?act)
 
 c_schema:hasIndex(?c, ?index)
 c_schema:hasOrigin(?act, ?stmt) 
-hasSource(?stmt, ?src) 
-hasLocationSuffix(?stmt, ?stlbl) 
+c_schema:hasSource(?stmt, ?src) 
+c_schema:hasLocationSuffix(?stmt, ?stlbl) 
 swrlb:stringConcat(?msg, "ActAfterEndOfBlockError: ", ?src, ?stlbl, "#", ?index, " is placed after end of block.")
  -> c_schema:message(c_schema:ERRORS, ?msg)
 
 
-# ActsPairMisorderError
-c_schema:Context(?c) 
-# c_schema:Block(?block) 
-c_schema:Statement(?stmt_1) 
-c_schema:Statement(?stmt_2) 
-c_schema:Act(?act1) 
-c_schema:Act(?act2) 
+# 3
+# ActsPairMisorderErro
+# # c_schema:Block(?block)
+# # c_schema:hasOrigin(?c, ?block)
+c_schema:Context(?c)
+c_schema:Statement(?stmt_1)
+c_schema:Statement(?stmt_2)
+c_schema:Act(?act1)
+c_schema:Act(?act2)
 
-c_schema:hasContext(?act1, ?c) 
-c_schema:hasContext(?act2, ?c) 
-c_schema:hasNext(?stmt_1, ?stmt_2) 
-# c_schema:hasOrigin(?c, ?block) 
-c_schema:hasOrigin(?act1, ?stmt_1) 
-c_schema:hasOrigin(?act2, ?stmt_2) 
+c_schema:hasContext(?act1, ?c)
+c_schema:hasContext(?act2, ?c)
+c_schema:hasNext(?stmt_1, ?stmt_2)
+c_schema:hasOrigin(?act1, ?stmt_1)
+c_schema:hasOrigin(?act2, ?stmt_2)
 c_schema:before(?act2, ?act1)
 
 c_schema:hasIndex(?c, ?index)
-hasSource(?stmt_1, ?src1) 
-hasSource(?stmt_2, ?src2) 
-swrlb:stringConcat(?msg, "ActAfterEndOfBlockError: ", ?src1, " and ", ?src2, " are placed in swapped order.")
+c_schema:hasSource(?stmt_1, ?src1) 
+c_schema:hasSource(?stmt_2, ?src2) 
+swrlb:stringConcat(?msg, "ActAfterEndOfBlockError: ", ?src1, " and ", ?src2, " are placed in reversed order.")
  -> c_schema:message(c_schema:ERRORS, ?msg)
