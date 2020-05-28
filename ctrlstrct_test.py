4c04148e6d7a016515e07df6ff8785944ddb37dc
den1s0v <hello-lumen@ya.ru>
2020-05-28 18:30:33 +0400

    Removed functions that no longer in use

--- ctrlstrct_test.py
+++ ctrlstrct_test.py
@@ -36,86 +36,6 @@ def log_print(*args, sep=" ", end="\n"):
 # def replace_print()	:
 # 	pass
 
-def run_tests_in_directory(directory) -> bool:
-	
-	raise "Deprecated"
-	
-	tests_fpath = os.path.join(directory, TESTS_FNM)
-	assert os.path.exists(tests_fpath), tests_fpath
-	with open(tests_fpath, encoding="utf8") as f:
-		tests_data = json.load(f)
-		
-	output_data = {}
-	run_report = {"succeded": 0, "failed": 0, "skipped": 0, "messages":[]}
-		
-	# iterate over the tests
-	for test_name in tests_data:
-		if test_name.startswith("-"):
-			output_data[test_name] = " == SKIPPED == "
-			run_report["skipped"] += 1
-		else:
-			print("Running test:", "[%s]"%test_name)
-			test_data = tests_data[test_name]
-			
-			fpath = resolve_path(test_data[ALG_FILE_FIELD], directory)
-			assert fpath, test_data[ALG_FILE_FIELD] + " not found ..."
-			with open(fpath, encoding="utf8") as f:
-				alg = f.read()
-
-			fpath = resolve_path(test_data[TRACE_FILE_FIELD], directory)
-			assert fpath, test_data[TRACE_FILE_FIELD] + " not found ..."
-			with open(fpath, encoding="utf8") as f:
-				tr = f.read()
-	
-			
-			try:
-				if SAVE_RDF:
-					ontology_file = test_name + "_output.rdf"
-					ontology_fpath = os.path.join(directory, ontology_file)
-					# onto.save(file=fpath, format='rdfxml')
-					# print("Saved RDF file: {} !".format(ontology_file))
-				else:
-					ontology_fpath = None
-
-				# Запуск !	
-				onto, mistakes = process_algtr(alg, tr, verbose=0, debug_rdf_fpath=ontology_fpath)
-				
-			except Exception as e:
-				msg = "Exception occured: %s: %s"%(str(type(e)), str(e))
-				ok = False
-				msg = ("[  ok]" if ok else "[FAIL]") + (" '%s'" % test_name) + ":\t" + msg
-				print(msg)
-				output_data[test_name] = msg
-				run_report["messages"].append(msg)
-				run_report["failed"] += 1
-				continue
-			
-			# записывать больше?
-			output_data[test_name] = mistakes
-				
-			# сравнить результат и эталон
-			# ...
-			ok, msg = compare_mistakes(test_data[REFERENCE_DATA_FIELD], mistakes)
-			msg = ("[  ok]" if ok else "[FAIL]") + (" '%s'" % test_name) + "\t:" + msg
-			print(msg)
-			# 	run_report = {"succeded": 0, "failed": 0, "messages":[]}
-			run_report["succeded" if ok else "failed"] += 1
-			run_report["messages"].append(msg)
-			
-		
-		fpath = os.path.join(directory, OUTPUT_FNM)
-		with open(fpath, "w", encoding="utf8") as f:
-			json.dump(output_data, f, indent=2)
-	
-		fpath = os.path.join(directory, REPORT_FNM)
-		with open(fpath, "w", encoding="utf8") as f:
-			f.write("\n".join(run_report["messages"]))
-			f.write("\n=============\n")
-			f.write("succeded: %d, failed: %d, total: %d.\n(skipped: %d)\n" % (run_report["succeded"], run_report["failed"], run_report["succeded"]+run_report["failed"], run_report["skipped"]))
-			
-	return run_report["failed"] == 0
-	
-
 def object_to_hashable(obj, discard_dict_keys=()):
 	if isinstance(obj, (tuple, str, int, float)):
 		return obj
@@ -174,45 +94,6 @@ def resolve_path(fname, directory='.'):
 		if os.path.exists(case_path):
 			return case_path
 	return None
-
-def run_test_for_alg_trace(test_data: dict, directory=TEST_DIR):
-	
-	raise "Deprecated"
-	
-	alg = test_data["algorithm"]
-	tr  = test_data["trace"]
-	test_name  = test_data["trace_name"]
-	
-	try:
-		if SAVE_RDF:
-			ontology_file = test_name + "_output.rdf"
-			ontology_fpath = os.path.join(directory, ontology_file)
-			# onto.save(file=fpath, format='rdfxml')
-			# print("Saved RDF file: {} !".format(ontology_file))
-		else:
-			ontology_fpath = None
-
-		# Запуск !	
-		onto, mistakes = process_algtr(alg, tr, verbose=0, debug_rdf_fpath=ontology_fpath, mistakes_as_objects=True)
-		
-	except Exception as e:
-		msg = "Exception occured: %s: %s"%(str(type(e)), str(e))
-		ok = False
-		msg = ("[  ok]" if ok else "[FAIL]") + (" '%s'" % test_name) + ":\t" + msg
-		log_print(msg)
-		
-		# debug only:
-		# raise e
-		
-		return False
-
-	# сравнить результат и эталон
-	# ...
-	ok, msg = validate_mistakes(tr, mistakes, onto)
-	msg = ("[  ok]" if ok else "[FAIL]") + (" '%s'" % test_name) + ":\t" + msg
-	log_print(msg)
-	
-	return ok
 
 
 def validate_mistakes(trace:list, mistakes:list, onto) -> (bool, str):
