f8e896f6e0a662757ed7eb831e7a99c95a17b5f5
Den1s0v <hello-lumen@ya.ru>
2020-07-28 02:14:01 +0400

    Add comments to outer text (just looks better with NPP syntax highlight)

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-import stardog
+a = set([1,2,3,4,5])
+b = set([3,4])
 
-help(stardog.Connection)
-# help(stardog.Admin)
+print(a - b)
+
+# import stardog
+
+# help(stardog.Connection)
+# # help(stardog.Admin)
 
