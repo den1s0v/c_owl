25bb8fa70ecb5c1f2ab07c51b7046abe94ee878a
den1s0v <hello-lumen@ya.ru>
2020-10-10 02:35:01 +0400

    A toy example of the web-service's backend operation

--- web_server.py
+++ web_server.py
@@ -1,12 +1,48 @@
 # web_server.py
 
-from flask import Flask
+# from flask import Flask
+
+from ctrlstrct_test import process_algorithm_and_trace_from_text
  
-app = Flask(__name__)
+# app = Flask(__name__)
  
-@app.route("/")
-def hello():
-    return "Hello, World!"
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
   
 if __name__ == "__main__":
-    app.run()
+	# app.run()
+	
+	text = """
+// алгоритм 10_while
+{
+пока красный -> ложь,ложь // ожидание
+	ждать
+ждать_секунды(3)
+идти
+}
+
+/* 
+SKIP____10_while 10 (с.3) проверка условия (While_Loop)
+10_while (с.3) проверка условия (While_Loop)
+*/ {
+началась программа
+	начался цикл ожидание 1-й раз
+		условие цикла (красный) выполнилось 1-й раз - истина // проверка условия
+		началась итерация 1 цикла ожидание
+			ждать выполнилось 1-й раз
+		закончилась итерация 1 цикла ожидание
+		условие цикла (красный) выполнилось 2-й раз - ложь
+	закончился цикл ожидание 1-й раз
+	ждать_секунды(3) выполнилось 1-й раз
+	идти выполнилось 1-й раз
+закончилась программа
+}
+
+	
+	"""
+	
+	feedback = process_algorithm_and_trace_from_text(text)
+	
+	from pprint import pprint
+	pprint(feedback)
