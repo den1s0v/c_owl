cc47a01d4ae30f2b68044a2a36d43a8ec0b33a6d
den1s0v <hello-lumen@ya.ru>
2020-10-12 02:12:35 +0400

    Mistakes convertation (server) and text backups via localStorage (client)

--- web_exp/static/script.js
+++ web_exp/static/script.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
 		    
 	     //    $('#new-tip').toggleClass("active");
 		    // $('#new-tip').slideToggle("slow");
-		    $('#new-tip').animate({ opacity: "show" }, 3000).animate({ opacity: "hide" }, 3000);
+		    // $('#new-tip').animate({ opacity: "show" }, 3000).animate({ opacity: "hide" }, 3000);
+		    $("#new").html("New (clear both fields)")
 		}
 		else
 		{
@@ -24,29 +25,101 @@ $(document).ready(function(){
 	    $('#alg').prop('disabled', true);
 	    $('#trace').prop('disabled', true);
 	    
-	    $('#new-tip').animate({ opacity: "hide" }, 3000);
+	    // $('#new-tip').animate({ opacity: "hide" }, 3000);
+	    $("#new").html("Edit (enable fields)")
 	     
 	    data = {
 	            alg: $("#alg").val(),
 	            trace: $("#trace").val()
 	        }
 	        
+	    save_fields(data)  // alg, trace -> localStorage
+	    
         $.ajax({
         	url: '/process', 
         	method: 'post',
         	dataType: 'json',
         	contentType: "application/json",
         	data: JSON.stringify(data),
+        	beforeSend: function(data){
+        		processing_callback("wait")
+        	},
         	success: function(data){
-        		alert("ajax OK");
-        		$('#status').html(JSON.stringify(data));
+        		// alert("ajax OK");
+        		// $('#status').html(JSON.stringify(data));
+        		processing_callback(data)
         	},
         	error: function(data){
-        		alert("ajax FAIL");
+        		// alert("ajax FAIL");
+        		processing_callback("fail")
         	}
         });
     });
     
+    // // pass "alg" or "trace"
+    // function load_field(field_name)
+    $("#alg").val(load_field("alg"))
+    $("#trace").val(load_field("trace"))
+
+    
     // alert('Waw!')
-    $('#new-tip').animate({ opacity: "hide" }, 3000).animate({ opacity: "show" }, 3000);
+    // $('#new-tip').animate({ opacity: "hide" }, 3000).animate({ opacity: "show" }, 3000);
 });
+
+
+function processing_callback(response="wait")
+{
+	if(response == "wait")
+	{
+		$('#status').html("Please wait ...");
+		return
+	}
+	
+	if(response == "fail")
+	{
+		$('#status').html("Server could not send a response. Try again later ...");
+		return
+	}
+	
+	if(response.messages)
+	{	
+		messages = response.messages.join("\n")
+		
+		mistakes_count = 0
+		if(response.mitakes)
+			mistakes_count = response.mistakes.length
+		
+		$('#status').html("Server responded:\n" + messages + '\n' + mistakes_count + ` mistakes.`
+			+ '\n' + JSON.stringify(response));
+	}	
+}
+
+
+////// local storage //////
+
+function save_fields(data=null)
+{
+	if(data === null)
+	{
+		data = {
+			alg: $("#alg").val(),
+			trace: $("#trace").val()
+		}
+	}
+	
+	localStorage.algtrace = JSON.stringify(data);
+}
+
+// pass "alg" or "trace"
+function load_field(field_name)
+{
+	str = localStorage.algtrace
+	if(!str)
+	{
+		return ''
+	}
+	
+	let data = JSON.parse( str );
+	return data[field_name]
+}
+
--- web_server.py
+++ web_server.py
@@ -1,22 +1,113 @@
 # web_server.py
 
-# from flask import Flask
+import re
+
+from flask import Flask, request, render_template, jsonify
 
 from ctrlstrct_test import process_algorithm_and_trace_from_text
- 
-# app = Flask(__name__)
+
+
+
+app = Flask(__name__, template_folder='web_exp/views', static_folder='web_exp/static',)
+
+# app.port = 2020
  
 # @app.route("/")
 # def hello():
 #     return "Hello, World!"
+
+@app.route('/demo')
+@app.route('/')
+@app.route('/index')
+@app.route('/index.html')
+def index():
+	return render_template('index.html')
+
+@app.route('/process', methods = ['POST'])
+def process_data():
+	# print(request.is_json)
+	# print(request.json)
+	# sleep(20)
+	try:
+		feedback = process_algorithm_and_trace_request(request.json)
+		return jsonify(feedback)
+	except Exception as ex:
+		raise ex
+		return dict(messages=[f"Error processing the request - {ex.__class__.__name__}: {str(ex)}"])
   
-if __name__ == "__main__":
-	# app.run()
+  
+def process_algorithm_and_trace_request(json):
+	assert "alg" in json
+	assert "trace" in json
 	
+	alg_text = json["alg"].strip()
+	trace_text = json["trace"].strip()
+	
+	# convert data from a webpage
+	
+	full_text = """algorithm user_alg
+{open_brace}
+{alg}
+{close_brace}
+
+user_alg {boolean_chain}user_trace
+{open_brace}
+{trace}
+{close_brace}	""".format(
+		alg=alg_text,
+		trace=trace_text,
+		boolean_chain=json["boolean_chain"] + " " if "boolean_chain" in json else '',
+		open_brace="{",
+		close_brace="}",
+	)
+	
+	alg_line_i = 3
+	trace_line_i = alg_line_i + len(alg_text.split("\n")) + 3
+	
+	# print(full_text)
+	print(dict(alg_line_i=alg_line_i, trace_line_i=trace_line_i))
+	
+	feedback = process_algorithm_and_trace_from_text(full_text)
+	
+	###
+	from pprint import pprint
+	pprint(feedback)
+	
+	formatted_feedback = {"messages": feedback["messages"], "mistakes": []}
+	
+	# convert result for use in a webpage
+	
+	if "mistakes" in feedback:
+		for m in feedback["mistakes"]:
+			d = {
+				# "text_line": m["text_line"][0] - trace_line_i  if m["text_line"] else -1,
+				"names": ', '.join([camelcase_to_snakecase(s) for s in m["classes"]]),
+				"act_abbr": ', '.join(str(o) for o in m["name"]),
+			}
+			if m["text_line"]:
+				d["text_line"] = m["text_line"][0] - trace_line_i,
+			if "should_be_before" in m and m["should_be_before"]:
+				line = m["should_be_before"][0].text_line
+				if line is not None:
+					d["should_be_before_line"] = line - trace_line_i
+			formatted_feedback["mistakes"].append(d)
+	
+	# return str(feedback)
+	return formatted_feedback
+
+
+__CAMELCASE_RE = re.compile(r"([a-z])([A-Z])")
+
+def camelcase_to_snakecase(s: str, sep='_') -> str:
+	return __CAMELCASE_RE.sub(lambda m: f"{m.group(1)}{sep}{m.group(2)}", s).lower()
+
+
+
+def debug():	
 	text = """
 // алгоритм 10_while
 {
-пока красный -> ложь,ложь // ожидание
+пока красный -> ложь,ложь // ожидание  (правильно истина,ложь)
 	ждать
 ждать_секунды(3)
 идти
@@ -38,11 +129,16 @@ SKIP____10_while 10 (с.3) проверка условия (While_Loop)
 	идти выполнилось 1-й раз
 закончилась программа
 }
-
-	
 	"""
 	
 	feedback = process_algorithm_and_trace_from_text(text)
 	
 	from pprint import pprint
 	pprint(feedback)
+
+
+if __name__ == "__main__":
+	# debug()
+	app.run(debug = 1)
+	
+
