3c615a9d6dba7f3600b35e85855a17aaf887d0f8
den1s0v <hello-lumen@ya.ru>
2020-12-17 14:31:53 +0400

    Adopting expressions logic to benchmark with reasoners

---
+++ __eval/expression_makeup.py
@@ -0,0 +1,171 @@
+# expressions-nikita.py
+
+import json
+
+from owlready2 import *
+
+from ctrlstrct_run import clear_ontology, uniqualize_iri
+from upd_onto import make_triple
+from __eval.expression_laws import inject_laws
+
+
+'''
+    String getName(int step, int index) {
+        return "op__" + step + "__" + index;
+    }
+'''
+
+def getName(step: int, index:int) -> str:
+    return "op__" + str(step) + "__" + str(index);
+
+'''
+    public List<BackendFact> getBackendFacts(List<String> expression) {
+        List<BackendFact> facts = new ArrayList<>();
+        int index = 0;
+        for (String token : expression) {
+            index++;
+            for (int step = 0; step <= expression.size(); ++step) {
+                String name = getName(step, index);
+                facts.add(new BackendFact(name, "rdf:type", "owl:NamedIndividual"));
+                facts.add(new BackendFact("owl:NamedIndividual", name, "index", "xsd:int", String.valueOf(index)));
+                facts.add(new BackendFact("owl:NamedIndividual", name, "step", "xsd:int", String.valueOf(step)));
+            }
+            facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "text", "xsd:string", token));
+            facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "complex_beginning", "xsd:boolean", Boolean.toString(token.equals("(") || token.equals("[") || token.equals("?"))));
+            facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "complex_ending", "xsd:boolean", Boolean.toString(token.equals(")") || token.equals("]") || token.equals(":"))));
+        }
+        facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "last", "xsd:boolean", "true"));
+        return facts;
+    }
+'''
+
+def write_expression(onto, expression: list):
+    with onto:
+        index = 0;
+        for token in expression:
+            # index++;
+            index += 1
+            for step in range(len(expression) + 1):
+                name = getName(step, index);
+                name = uniqualize_iri(onto, name)
+                # facts.add(new BackendFact(name, "rdf:type", "owl:NamedIndividual"));
+                obj = Thing(name)
+                
+                # facts.add(new BackendFact("owl:NamedIndividual", name, "index", "xsd:int", String.valueOf(index)));
+                make_triple(obj, onto["index"], index)
+                # facts.add(new BackendFact("owl:NamedIndividual", name, "step", "xsd:int", String.valueOf(step)));
+                make_triple(obj, onto["step"], step)
+            
+                if step == 0:
+                    # facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "text", "xsd:string", token));
+                    make_triple(obj, onto["text"], token)
+                    # facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "complex_beginning", "xsd:boolean", Boolean.toString(token.equals("(") || token.equals("[") || token.equals("?"))));
+                    make_triple(obj, onto["complex_beginning"], bool(token in '([?'))
+                    # facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "complex_ending", "xsd:boolean", Boolean.toString(token.equals(")") || token.equals("]") || token.equals(":"))));
+                    make_triple(obj, onto["complex_ending"], bool(token in ')]:'))
+                    last_obj_with_step_0 = obj
+        
+        # facts.add(new BackendFact("owl:NamedIndividual", getName(0, index), "last", "xsd:boolean", "true"));
+        make_triple(last_obj_with_step_0, onto["last"], True)
+    
+        
+def test_items_to_expressions(test_items: list) -> list:
+    field = 'expression'
+    exprs = []
+    for d in test_items:
+        if field in d and d[field]:
+            data = d[field]
+            # in_list = False
+            for old_data in exprs:
+                if data == old_data:
+                    # print('filtered:', data)
+                    break
+            else:
+                exprs.append(data)
+    
+    return exprs
+
+def iterate_over_expr_chains(expressions: 'iterable of lists', preferred_step=20, sep=',') -> iter:
+    expressions = list(expressions)
+    joined_exprs = expressions[0]  # , sep]
+    for i in range(1, len(expressions)):
+        modulo = len(joined_exprs) % preferred_step
+        need_more = preferred_step - modulo if modulo > 0 else 0
+        can_add = len(expressions[i]) + 1
+        if can_add <= need_more:
+            # add one more and continue
+            joined_exprs.append(sep)
+            joined_exprs.extend(expressions[i])
+            continue
+        modulo_can_be = (modulo + can_add) % preferred_step
+        
+        if modulo_can_be < need_more:
+            # add one more before yield
+            joined_exprs.append(sep)
+            joined_exprs.extend(expressions[i])
+            yield joined_exprs
+        else:
+            # yield and then add one more
+            yield joined_exprs
+            joined_exprs.append(sep)
+            joined_exprs.extend(expressions[i])
+    
+    yield joined_exprs
+            
+            
+    
+
+def load_all_test_items() -> list:
+    paths = [
+        r"c:\D\Work\YDev\CompPr\CompPrehension\src\test\resources\has-operand-test-data.json",
+        r"c:\D\Work\YDev\CompPr\CompPrehension\src\test\resources\simple-ontology-test-data.json",
+        r"c:\D\Work\YDev\CompPr\CompPrehension\src\test\resources\before-test-data.json",
+    ]
+    test_items = []
+    for path in paths:
+        test_items += load_test_items_from_file(path)
+    return test_items
+        
+    
+def load_test_items_from_file(filepath) -> list:
+    with open(filepath) as file:
+        return json.load(file)
+
+
+def bad_func__iterate_over_test_expr_ontologies(max_size=None, inject_swrl=False):
+    data = test_items_to_expressions(load_all_test_items())
+    
+    for chain in iterate_over_expr_chains(data):
+        if len(chain) > max_size:
+            break  # and thus return
+        
+        onto = get_ontology('penskoy/expressions')
+        clear_ontology(onto, keep_tbox=False)
+        
+        inject_laws(onto, omit_swrl=not inject_swrl)
+        write_expression(onto, chain)
+        
+        yield onto
+
+def prepare_ontology(expr_chain, inject_swrl=False, iri='http://penskoy.n/expressions'):
+        onto = get_ontology(iri)
+        clear_ontology(onto, keep_tbox=False)
+        inject_laws(onto, omit_swrl=not inject_swrl)
+        write_expression(onto, expr_chain)
+        return onto
+    
+
+
+def main():
+    data = test_items_to_expressions(load_all_test_items())
+    # from pprint import pprint
+    # pprint(data)
+    
+    for chain in iterate_over_expr_chains(data[:100]):
+        L = len(chain)
+        print(L)
+        # if L < 100:
+        #     print(' '.join(chain))
+
+# if __name__ == '__main__':
+#     main()
---
+++ run_exprs.py
@@ -0,0 +1,166 @@
+from __eval.expression_makeup import *
+from ctrlstrct_run import *
+
+# main()
+    
+DATA = test_items_to_expressions(load_all_test_items())
+PREV_COUNT = {}
+
+
+def proccess_onto_with_reasoner(reasoning, count=30):
+    
+    # get only the first result from generator
+    for chain in iterate_over_expr_chains(DATA, count):
+        expr_chain = chain
+        break
+        
+    if reasoning in PREV_COUNT and len(expr_chain) == PREV_COUNT[reasoning]:
+        return None
+        
+    PREV_COUNT[reasoning] = len(expr_chain)
+    
+    if reasoning == "pellet":
+        
+        onto = prepare_ontology(expr_chain, inject_swrl=True)
+        
+        if True:
+            debug_rdf_fpath = 'exprs_dump.rdf'  # +"_ext.rdf"
+            onto.save(file=debug_rdf_fpath, format='rdfxml')
+            debug_rdf_fpath = 'exprs_dump.n3'
+            onto.save(file=debug_rdf_fpath, format='ntriples')
+            print(f"Saved RDF file: {debug_rdf_fpath} !")
+
+
+        print(">_ running Pellet ...")
+        
+        # if _eval_max_traces is not None:
+        measure_stats_for_pellet_running()
+        
+        start = timer()
+        
+        with onto:
+            # запуск Pellet
+            try:
+                sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True, debug=0)
+            except Exception as e:
+                print(e)
+            
+        end = timer()
+        seconds = end - start
+        time_report = "   Time elapsed: %.3f s." % seconds
+        print(">_ Pellet finished")
+        print(time_report)
+        
+        # if _eval_max_traces is not None:
+        run_stats = get_pellet_run_stats()
+        run_stats.update({"wall_time": seconds})
+        run_stats.update({"count": len(expr_chain)})
+        return run_stats
+
+        # if debug_rdf_fpath:
+        #     onto.save(file=debug_rdf_fpath+"_ext.rdf", format='rdfxml')
+        #     print(f"Saved RDF file: {debug_rdf_fpath}_ext.rdf !")
+            
+    onto = prepare_ontology(expr_chain, inject_swrl=False)
+    
+            
+    if reasoning == "prolog":
+        name_in = "pl_in_expr.rdf"
+        name_out = "pl_out_expr.rdf"
+        onto.save(file=name_in, format='rdfxml')
+        
+        eval_stats = run_swiprolog_reasoning(name_in, name_out, verbose=1, command_name="run_ontology")
+        
+        # if _eval_max_traces is not None:
+        eval_stats.update({"count": len(expr_chain)})
+        return eval_stats
+        
+        # clear_ontology(onto)
+        # onto = get_ontology("file://" + name_out).load()
+        # seconds = eval_stats['wall_time']
+        
+            
+    if reasoning in ('sparql', 'jena'):
+        name_in = f"{reasoning}_in_expr.rdf"
+        name_out = f"{reasoning}_out_expr.n3"
+        onto.save(file=name_in, format='rdfxml')
+        
+        rules_path = {
+            'jena': "jena/all_for_exprs.rules",
+            'sparql': "expr_penskoy.ru", 
+        }[reasoning]
+        
+        eval_stats = run_jena_reasoning(name_in, name_out, reasoning_mode=reasoning, verbose=1, rules_path=rules_path)
+        
+        # if _eval_max_traces is not None:
+        eval_stats.update({"count": len(expr_chain)})
+        return eval_stats
+        
+        # clear_ontology(onto)
+        # onto = get_ontology("file://" + name_out).load()
+        # seconds = eval_stats['wall_time']
+    raise ValueError(reasoning)
+        
+    
+def eval_expressions():
+    eval_results = []
+    # 46
+    for n in range(7, 10 + 1, 6):
+        # reasoners = ("pellet", )
+        # reasoners = ("prolog", ); alg_trs = alg_trs[:22]  # !!!
+        reasoners = ("prolog", )
+        # reasoners = ("jena", "sparql")
+        # reasoners = ("jena", "prolog", "sparql")
+            
+        for reasoning_type in reasoners:
+            print(' >  >  >  >  >  >  >  >  >  >  >  >  > ')
+            print(f"  Running {n} operands with {reasoning_type}")
+            print(' <  <  <  <  <  <  <  <  <  <  <  <  < ')
+            
+            eval_result = proccess_onto_with_reasoner(count=n, reasoning=reasoning_type)
+            
+            if eval_result is None:
+                continue
+            
+            eval_item = {
+                'dataset': 'penskoy-expressions',
+                'reasoner': reasoning_type,
+                'count': n,
+            }
+            eval_item.update(eval_result)
+            
+            # dump current result
+            with open('partial_eval_expr.txt', "a") as file:
+                file.write(str(eval_item))
+                file.write('\n')
+            
+            eval_results.append(eval_item)
+            
+        # break
+            
+    # dump full result
+    with open('saved_eval_expr.txt', "a") as file:
+        for eval_item in eval_results:
+            file.write(str(eval_item))
+            file.write('\n')
+            
+    print(' ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^ ')    
+    print("Expr eval finished.") 
+    # exit()
+
+
+def convert_rules():
+    from rule_converter import to_prolog, to_jena, to_sparql
+    from __eval.expression_laws import get_owl_swrl_laws
+    _owl, swrl = get_owl_swrl_laws()
+    to_prolog(swrl, out_path='expr_penskoy.pl', iri_prefix="http://penskoy.n/expressions#")
+    # to_jena(swrl, out_path='expr_penskoy.jena_rules')
+    # to_sparql(swrl, out_path='expr_penskoy.ru')
+
+
+def main():
+    eval_expressions()
+    # convert_rules()
+
+if __name__ == '__main__':
+    main()
