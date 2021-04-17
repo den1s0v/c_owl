3776c410ad36f4189943a6c63986acbab85f022d
den1s0v <hello-lumen@ya.ru>
2021-04-18 02:51:39 +0400

    Add commandline parameters: host=, port=

--- web_server.py
+++ web_server.py
@@ -1,11 +1,12 @@
 # web_server.py
 
 import re
+import sys
 
 from flask import Flask, request, render_template, jsonify, url_for, redirect
 
 # # import the flask extension
-# from flask_caching import Cache  
+# from flask_caching import Cache
 
 from ctrlstrct_test import process_algorithm_and_trace_from_text, process_algorithm_and_trace_from_json, make_act_json, process_algorithms_and_traces, add_styling_to_trace
 from ctrlstrct_run import make_trace_for_algorithm
@@ -24,7 +25,7 @@ def create_app():
 
 	app = Flask(__name__, template_folder='web_exp/views', static_folder='web_exp/static',)
 
-	# bind the cache instance on to the app 
+	# bind the cache instance on to the app
 	# cache.init_app(app)
 
 	@app.route('/index.html')
@@ -32,35 +33,35 @@ def create_app():
 	@app.route('/')
 	def index():
 		return render_template('index.html')
-		
+
 	@app.route('/demo/')
 	@app.route('/iswc/demo/')
 	def demo():
 		return render_template('demo.html')
-		
+
 	@app.route('/api_test/')
 	@app.route('/test/')
 	def api_test():
 		return render_template('api_test.html')
-		
+
 	@app.route('/blockly_test/<lang>/')
 	@app.route('/blockly_test/')
 	def blockly_test(lang='en'):
 		return render_template('blockly_test.html', language=lang)
-		
+
 	@app.route('/favicon.ico')
 	def icon():
 		url = url_for('static', filename='fireball.png')
 		return redirect(url)
 		# return render_template('index.html')
-		
+
 	@app.errorhandler(404)
 	def http_404_handler(error):
 		# return "<p>HTTP 404 Error Encountered</p>", 404
 		if not 'static' in request.url:
 			url = url_for('static', filename=request.path)
 			return redirect(url)
-		
+
 		url = url_for('index')
 		return '<p>HTTP 404 Error Encountered</p><p>Requested URL: %s</p>Get me <a href="%s">Home</a>' % (request.url, url), 404
 		# return redirect(url)
@@ -76,7 +77,7 @@ def create_app():
 		# print(request.json)
 		try:
 			assert 'algorithm_text' in request.json, 'Bad json: No "algorithm_text" key in JSON payload!'
-			
+
 			algorithm_text = request.json['algorithm_text']
 			user_language = request.json.get('user_language', 'en')
 			set_target_lang(user_language)
@@ -84,32 +85,32 @@ def create_app():
 				res = create_algorithm_from_blockly_xml(algorithm_text)
 			else:
 				res = create_algorithm_from_text(algorithm_text.splitlines())
-		
+
 			# if error
 			assert not isinstance(res, str), res
-				
+
 			if isinstance(res, AlgorithmParser):
 				###
 				# from pprint import pprint
 				# pprint(res.algorithm)
-				
+
 				# trace_json = ()
 				trace_json = make_trace_for_algorithm(res.algorithm)
 				trace_json = add_styling_to_trace(res.algorithm, trace_json, user_language)
-		
+
 				# if error
 				assert not isinstance(trace_json, str), trace_json
 
 				algorithm_tags = syntax.algorithm_to_tags(res.algorithm, user_language, request.json.get('syntax', 'C'))
 				algorithm_html = styling.to_html(algorithm_tags)
-				
+
 				return dict(
-					syntax_errors=(), 
+					syntax_errors=(),
 					algorithm_json=res.algorithm,
 					algorithm_as_html=algorithm_html,
 					trace_json=trace_json
 				)
-			
+
 		except Exception as ex:
 			res = f"{type(ex)}: {ex}"
 			print_exception(ex)
@@ -117,14 +118,14 @@ def create_app():
 			print(request.json)
 			print("The error above reported as response, continue.")
 			return dict(
-				syntax_errors=(res,), 
+				syntax_errors=(res,),
 				algorithm_json=None,
 				algorithm_as_html=None,
 				trace_json=()
 			)
 
 		return dict(syntax_errors=("Server error: /creating_task command is not implemented",))  # debug
-	
+
 
 
 	@app.route('/verify_trace_act', methods=['POST'])
@@ -132,22 +133,22 @@ def create_app():
 		### print(request.json)
 		try:
 			assert 'algorithm_json' in request.json, 'Bad json: No "algorithm_json" key in JSON payload!'
-			
+
 			user_language = request.json.get('user_language', 'en')
 			set_target_lang(user_language)
-		
+
 			# extend the trace
 			res = make_act_json(**{
 				k:v for k,v in request.json.items() if k in ("algorithm_json", "algorithm_element_id", "act_type", "existing_trace_json", "user_language")
 				})
-			
+
 			# if error
 			assert not isinstance(res, str), res
 
 			if isinstance(res, list):
-				
+
 				algorithm_json = request.json["algorithm_json"]
-				
+
 				# verify the obtained trace with reasoner
 				full_trace = res
 				alg_tr = {
@@ -159,19 +160,19 @@ def create_app():
 				}
 				# update acts data (inplace): write mistake explanations
 				_mistakes, err_msg = process_algorithms_and_traces([alg_tr], write_mistakes_to_acts=True)
-				
+
 				assert not err_msg, err_msg
-					
+
 				### print("After reasoning: ", *full_trace, sep='\n *\t')
-				
+
 				algorithm_tags = syntax.algorithm_to_tags(algorithm_json, user_language, request.json.get('syntax', 'C'), existing_trace=full_trace)
 				algorithm_html = styling.to_html(algorithm_tags)
-				
+
 				return dict(
 					full_trace_json=full_trace,  ## res,
 					algorithm_json=algorithm_json,  # pass back
 					algorithm_as_html=algorithm_html,
-					processing_errors=(), 
+					processing_errors=(),
 				)
 
 		except Exception as ex:
@@ -180,13 +181,13 @@ def create_app():
 			print("request.json :")
 			print(request.json)
 			return dict(
-				processing_errors=(res,), 
+				processing_errors=(res,),
 				full_trace_json=(),
 				algorithm_json=None,
 				algorithm_as_html=None,
 			)
 			print("The error above reported as response, continue.")
-			
+
 		return dict(trace_line_json={"as_string": "Dummy act line!", "id": 49, "loop": "waiting", "executes": 7, "gen": "he", "phase": "started", "_n": 4})  # debug
 
 
@@ -223,12 +224,12 @@ def print_exception(ex, print_args=()):
 def process_algorithm_and_trace_as_text_request(json):
 	assert "alg" in json
 	assert "trace" in json
-	
+
 	alg_text = json["alg"].strip()
 	trace_text = json["trace"].strip()
-	
+
 	# convert data from a webpage
-	
+
 	full_text = """algorithm user_alg
 {open_brace}
 {alg}
@@ -244,37 +245,37 @@ user_alg {boolean_chain}user_trace
 		open_brace="{",
 		close_brace="}",
 	)
-	
+
 	alg_line_i = 3
 	trace_line_i = alg_line_i + len(alg_text.split("\n")) + 3
-	
+
 	print(full_text)
 	print(dict(alg_line_i=alg_line_i, trace_line_i=trace_line_i))
-	
+
 	feedback = process_algorithm_and_trace_from_text(full_text)
-	
+
 	###
 	# from pprint import pprint
 	# pprint(feedback)
-	
+
 	formatted_feedback = {"messages": [], "mistakes": []}
-	
+
 	# convert result for use in a webpage
 	def convert_line_index(i: int):
 		return i - trace_line_i
-	
+
 	def convert_line_index_in_str(s):
 		if "line" in s or "строк" in s:
 			res = __LINE_IDNEX_RE.sub(lambda m: f"{m.group(1)} {convert_line_index(int(m.group(2)))}", s)
 			print("Replaced line index: ", s, "->", res)
 			s = res
 		return s
-	
+
 	if "messages" in feedback:
 		for s in feedback["messages"]:
 			s = convert_line_index_in_str(s)
 			formatted_feedback["messages"].append(s)
-	
+
 	if "mistakes" in feedback:
 		for m in feedback["mistakes"]:
 			d = {
@@ -291,7 +292,7 @@ user_alg {boolean_chain}user_trace
 				if line is not None:
 					d["should_be_before_line"] = convert_line_index(line)
 			formatted_feedback["mistakes"].append(d)
-	
+
 	# return str(feedback)
 	return formatted_feedback
 
@@ -301,10 +302,10 @@ def process_algorithm_and_trace_as_json_request(json):
 	###
 	# from pprint import pprint
 	# pprint(feedback)
-	
+
 	formatted_feedback = {"messages": [], "mistakes": []}
 	formatted_feedback["messages"] = feedback["messages"]
-	
+
 	if "mistakes" in feedback:
 		for m in feedback["mistakes"]:
 			d = {
@@ -319,7 +320,7 @@ def process_algorithm_and_trace_as_json_request(json):
 				if line is not None:
 					d["should_be_before_line"] = line
 			formatted_feedback["mistakes"].append(d)
-	
+
 	# return str(feedback)
 	return formatted_feedback
 
@@ -332,12 +333,26 @@ def camelcase_to_snakecase(s: str, sep='_') -> str:
 
 
 if __name__ == "__main__":
+	host = None
+	port = None
+
+	# check if something config passed from command line
+	for arg in sys.argv[1:]:
+		if arg.startswith('host='):
+			host = arg.replace('host=', '')
+			print("Using host specified on command line:", host)
+		elif arg.startswith('port='):
+			port = arg.replace('port=', '')
+			print("Using port specified on command line:", port)
+
+
 	app = create_app()  # Flask app
-	
-	host = 'localhost' if RUN_LOCALLY else "109.206.169.214"
-	
+
+	host = host or ('localhost' if RUN_LOCALLY else "109.206.169.214")
+	port = port or 2020
+
 	if DEBUG:
-		app.run(debug = DEBUG, host=host, port=2020)
-	else:	
+		app.run(debug=DEBUG, host=host, port=port)
+	else:
 		from waitress import serve
-		serve(app, host=host, port=2020)
+		serve(app, host=host, port=port)
