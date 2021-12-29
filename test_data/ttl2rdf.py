26654c4211e81c2ab13cc55dbd21035735af5c74
den1s0v <hello-lumen@ya.ru>
2021-12-29 19:02:57 +0300

    Question templates preparation: adding boundaries for each action

--- test_data/ttl2rdf.py
+++ test_data/ttl2rdf.py
@@ -2,6 +2,7 @@ import os.path
 from glob import glob
 
 import rdflib  # pip install rdflib
+from rdflib import RDF
 
 SEARCH_PATTERN = '*.ttl'
 
@@ -13,12 +14,32 @@ FORMAT_OUT = "xml"
 EXT_OUT = ".rdf"
 # EXT_OUT = ".full.rdf"
 
-# OVERWRITE_ANYWAY = True
-OVERWRITE_ANYWAY = False
+OVERWRITE_ANYWAY = True
+# OVERWRITE_ANYWAY = False
 
 INJECT_RDF = [
 	# r'c:/D/Work/YDev/CompPr/c_owl/jena/control-flow-statements-domain-schema.rdf',
 ]
+
+
+NS_CODE = 'http://vstu.ru/poas/code#'
+
+# !! keep list of actions up to date!
+action_classes = { rdflib.term.URIRef(NS_CODE + name)
+					for name in 'action sequence expr stmt alternative if else-if else func func_call loop while_loop do_while_loop do_until_loop for_loop foreach_loop'.split() }
+
+def patch(g):
+	# insert boundaries for actions
+	boundary_class = rdflib.term.URIRef(NS_CODE + "boundary")
+	for s, o in g.subject_objects(RDF.type):
+		if o in action_classes:
+			for prop_name in ("begin_of", "end_of"):
+				bound = rdflib.term.URIRef(s.replace(NS_CODE, NS_CODE + prop_name + "_"))
+				g.add((bound, RDF.type, boundary_class))
+				prop = rdflib.term.URIRef(NS_CODE + prop_name)
+				g.add((bound, prop, s))
+	return g
+
 
 
 def convert(file_in, file_out):
@@ -59,6 +80,7 @@ if __name__ == '__main__':
 			# convert(fp, target_filepath)
 			g = read_rdf(fp, rdf_format=FORMAT_IN)
 			g += inject_g
+			patch(g)
 			g.serialize(target_filepath, format=FORMAT_OUT)
 		except Exception as e:
 			print("#################")
