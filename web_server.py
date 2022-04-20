6a06fcfed096e669d8b10fffee23bba75be5bfe3
den1s0v <hello-lumen@ya.ru>
2022-04-20 19:15:17 +0300

    correct typo in variable name

--- web_server.py
+++ web_server.py
@@ -238,9 +238,9 @@ def process_algorithm_and_trace_as_text_request(json):
 	assert "trace" in json
 
 	# should create var on first execution
-	if '__LINE_IDNEX_RE' not in globals():
-		global __LINE_IDNEX_RE
-		__LINE_IDNEX_RE = re.compile(r"(line|строк\w+)\s*(\d+)")
+	if '__LINE_INDEX_RE' not in globals():
+		global __LINE_INDEX_RE
+		__LINE_INDEX_RE = re.compile(r"(line|строк\w+)\s*(\d+)")
 
 	alg_text = json["alg"].strip()
 	trace_text = json["trace"].strip()
@@ -283,7 +283,7 @@ user_alg {boolean_chain}user_trace
 
 	def convert_line_index_in_str(s):
 		if "line" in s or "строк" in s:
-			res = __LINE_IDNEX_RE.sub(lambda m: f"{m.group(1)} {convert_line_index(int(m.group(2)))}", s)
+			res = __LINE_INDEX_RE.sub(lambda m: f"{m.group(1)} {convert_line_index(int(m.group(2)))}", s)
 			print("Replaced line index: ", s, "->", res)
 			s = res
 		return s
